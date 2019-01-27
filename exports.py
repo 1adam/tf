dc463feb4dd7fd6f57c8de541a0f5f04a64996ea
1adam <1adam@users.noreply.github.com>
2019-01-27 16:53:47 +0000

    add find-latest-bionic-ubuntu func

--- exports.py
+++ exports.py
@@ -1,6 +1,19 @@
 from __future__ import print_function
 import json, boto3
 
+def find_latest_bionic_ami():
+  client = boto3.client('ec2')
+  resp = client.describe_images(
+    Filters=[
+      { 'Name': 'architecture', 'Values': ['x86_64'] },
+      { 'Name': 'owner-id', 'Values': ['099720109477'] },
+      { 'Name': 'name', 'Values': ['ubuntu/images/hvm-ssd/ubuntu-bionic-18.04*'] },
+      { 'Name': 'description', 'Values': ['*LTS*'] }
+    ])
+  for ami in resp['Images']:
+    final = sorted(resp['Images'], key=lambda x: x['CreationDate'], reverse=True)
+    return final[0]['ImageId']
+
 def verify_msg( incMsg ):
     for field in ["creator_name","type","environment"]:
         try:
@@ -19,17 +32,25 @@ def parse_msg( incMsg ):
     instType = types_map[ incMsg['type'] ]
     deployEnv = incMsg['environment']
 
+    ec2_exist = boto3.resource('ec2').instances.filter(
+      Filters=[{'Name': 'instance-state-name', 'Values': ['running'] }, {'Name': 'tag:Name', 'Values': [deployEnv + "_" + madeBy]} ])
+    for inst_exist in ec2_exist:
+        print('found existing vm called ', deployEnv+"_"+madeBy)
+        return 2
+
     ec2_inst = boto3.client('ec2')
 
+    latest_bionic_ami = find_latest_bionic_ami()
+
     resp = ec2_inst.run_instances(
-        ImageId='ami-0b86cfbff176b7d3a',
+        ImageId=latest_bionic_ami,
         InstanceType=instType,
         MaxCount=1,
         MinCount=1,
         TagSpecifications=[{
             'ResourceType': 'instance',
             'Tags': [
-                {"Key": "Name", "Value": deployEnv + "_" + madeBy }
+                {"Key": "Name", "Value": deployEnv+"_"+madeBy }
             ]
         }]
     )
